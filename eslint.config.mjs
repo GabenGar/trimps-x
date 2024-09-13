ae1ef0ddee400f14f8d2e2c65e9afcdf16ce8732
gabengar <gabengar@protonmail.com>
2024-09-13 20:43:58 +0500

    install prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const config = [
-  {files: ["./src/**/*.{js,mjs,cjs,ts,jsx,tsx}"], ignores: ["./dist/**", "./public/**"],settings: { react: { version: '18.3.1' } }},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["./src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["./dist/**", "./public/**"],
+    settings: { react: { version: "18.3.1" } },
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-]
+  eslintConfigPrettier,
+];
 
 export default config;
