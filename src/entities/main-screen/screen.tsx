5d176cf08eda987e4ba5ceef1bad4b1af7e93126
gabengar <gabengar@protonmail.com>
2024-10-10 21:18:53 +0500

    fix: style reference errors

--- src/entities/main-screen/screen.tsx
+++ src/entities/main-screen/screen.tsx
@@ -1,10 +1,14 @@
 import { BottomRow } from "./bottom-row";
 import { Settings } from "./settings";
 import { TopRow } from "./top-row";
+import bgImage from "#assets/images/bg2.png";
 
 export function MainScreen() {
   return (
-    <div id="wrapper" style={{ background: "url('/css/bg2.png') center repeat-x" }}>
+    <div
+      id="wrapper"
+      style={{ background: `url('${bgImage.src}') center repeat-x` }}
+    >
       <div id="innerWrapper">
         <div id="warnings"></div>
 
