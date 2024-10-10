33bbc9cbba1abbe03c8d9bc1317cb3eacac36de9
gabengar <gabengar@protonmail.com>
2024-10-10 21:10:14 +0500

    chore: format style files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="css/bootstrap.css" />
         <link rel="stylesheet" href="fonts/icomoon/style.css" />
-        <link rel="stylesheet" href="css/style.css" />
       </Head>
       <body>
         <Main />
